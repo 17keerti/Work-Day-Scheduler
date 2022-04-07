d1919877a39b90abd7ba1612f2e22f5abba60906
Keerti Chaudhary <17keerti@gmail.com>
2022-04-07 00:54:31 -0700

    save data to local storage on save button

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,23 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 
+var saveButton =$('.saveBtn');
+
+
+  
+  
+
+  
+
+
+saveButton.on("click", function() {
+  var plan = $(this).siblings(".textarea").first().val();
+  var time = $(this).siblings(".hour").first().text();
+  console.log(plan);
+  console.log(time);
+
+  localStorage.setItem(plan,time);
+});
+
+
 
