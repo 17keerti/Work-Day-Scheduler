392bdc08501c79be9d2c44ded376b8710d3293ca
Keerti Chaudhary <17keerti@gmail.com>
2022-04-07 19:03:47 -0700

    add function to color code textarea w.r.t time

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,39 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 
-var saveButton =$('.saveBtn');
-
-
-  
-  
-
-  
-
-
-saveButton.on("click", function() {
+var saveButton = $('.saveBtn');
+saveButton.on("click", function () {
   var plan = $(this).siblings(".textarea").first().val();
   var time = $(this).siblings(".hour").first().text();
   console.log(plan);
   console.log(time);
 
-  localStorage.setItem(plan,time);
+  localStorage.setItem(time, plan);
 });
 
 
+function timeBlockColor() {
+  var currentHour = moment().hour();
+  console.log(currentHour);
 
+  $(".hour").each(function () {
+    
+    var time = $(this).text();
+    console.log("here" +time);
+    var splitTime = time.split();
+    var blockTime = parseInt(splitTime[0]);
+    if (splitTime[1] == 'PM') {
+      blockTime = blockTime + 12;
+    }
+    console.log(blockTime);
+    if (blockTime > currentHour) {
+      $(this).siblings(".textarea").first().addClass("future");
+    } else if (blockTime === currentHour) {
+      $(this).siblings(".textarea").first().addClass("present");
+    } else {
+      $(this).siblings(".textarea").first().addClass("past");
+    }
+  })
+}
+
+timeBlockColor();
